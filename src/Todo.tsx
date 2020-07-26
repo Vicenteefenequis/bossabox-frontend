671ed7ce12e4b38102fdbae44bd79e7d66c25078
Vicenteefenequis <efenequis19981@gmail.com>
2020-07-25 21:34:18 -0300

    Update Interface for Responsive

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,91 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
+  InputAdornment,
+  InputLabel,
   Input,
-  FormGroup,
-  FormControlLabel,
+  FormControl,
   Checkbox,
-  Button
-} from '@material-ui/core'
+} from "@material-ui/core";
+import { Search, Add } from "@material-ui/icons";
+import "./Todo.css";
+import api from "./services/api";
+import Card from "./components/Card/index";
+import Header from "./components/Header/index";
 
-import {
-  Add,
-  Search
-} from '@material-ui/icons'
-import './Todo.css'
-import api from './services/api'
-import Card from './components/Card/index'
-import Header from './components/Header/index'
-
-
-interface Items{
-  id: number,
-  title:string,
-  link:string,
-  description:string,
-  tags:[]
+interface Items {
+  id: number;
+  title: string;
+  link: string;
+  description: string;
+  tags: [];
 }
 
 const Todo: React.FC = () => {
-  const [items,setItems] = useState<Items[]>([]);
-  const [showModal,setShowModal] = useState(false);
-  useEffect(()=>{
-    api.get('/tools').then(response => {
-      console.log(response.data)
+  const [items, setItems] = useState<Items[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    api.get("/tools").then((response) => {
+      console.log(response.data);
       setItems(response.data);
-    })
-  },[])
+    });
+  }, []);
 
-  function onHideModal(){
-    setShowModal(false)
+  function onHideModal() {
+    setShowModal(false);
   }
 
   return (
     <>
-      <Header title="VUTTR" description="Very Useful Tools to Remember"/>
-      <div className="readOnly">
+      <Header title="VUTTR" description="Very Useful Tools to Remember" />
+      <div className="fieldSearch">
         <div className="search">
-          <div className="search-icon">
-            <Search className="search-icon2" />
-          </div>
-        <Input
-        placeholder="Search..."
-        classes={{
-          root:"input-root",
-          input:"input-input"
-        }}
-        inputProps={{"aria-label":"Search"}}
-        />
-          
+          <FormControl className="classes.margin">
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Oque Procura?
+            </InputLabel>
+            <Input
+              id="input-with-icon-adornment"
+              startAdornment={
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <Checkbox />
+          Search is tags only
         </div>
-        <div className="cb">
-        <FormGroup row>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    value="checked"
-                    color="secondary"
-                  />
-                }
-                label="search in tags only"
-              />
-            </FormGroup>
+        <div className="add-button">
+          <Add />
+          Add
         </div>
-    
-        <div className="addnew">
-            <Button
-                  aria-label="Add"
-                  className="add-button"
-                  onClick={()=>{setShowModal(true)}}
-            >
-                  <Add className="add-icon" />
-                  Adicionar
-            </Button>
-        </div>
-      
       </div>
       <Card items={items} />
     </>
   );
-}
+};
 
 export default Todo;
